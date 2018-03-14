509ecf81b1b26f3b105b2493250a41b7fa2de112
Andriy Padalka <andr.padalka@gmail.com>
2018-03-14 09:47:45 +0300

    a.txt was removed from local master, \n was added stl_11.cpp, readme.txt

---
+++ stl_11.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    {
+        vector<int> vc;
+        list<int>  lst;
+        for(int i = 0; i < 10; ++i)
+        {
+            vc.push_back(rand() % 21);
+            lst.push_back(rand() % 21);
+        }
+        cout<<std::distance(vc.begin(), vc.end())<<endl;
+        cout<<std::distance(vc.end(), vc.begin())<<endl;
+
+        cout<<std::distance(lst.begin(), lst.end())<<endl;
+        cout<<std::distance(lst.end(), lst.begin())<<endl;
+    }
+
+    //cout << "Hello World!" << endl;
+    return 0;
+}
